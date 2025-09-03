6b90f978383d7662ff332dd150e54c026308899c
Ayush-delta <ayushratna2012@gmail.com>
2025-09-04 00:53:22 +0530

    Add cpp code

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+int a = 4;
+    // int b = 5;
+    // float pi = 3.14;
+    // cout << "\nthis is a test. Here is the value of a is "<< a <<" and b is " << b ;
+    // cout << " \nand the value of pi is " << pi << endl;
