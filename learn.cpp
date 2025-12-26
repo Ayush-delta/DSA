da744682f15585a6e899deb4639aa5937bd6b8cd
Ayush-delta <ayushratna2012@gmail.com>
2025-12-27 01:48:12 +0530

    Input taking

---
+++ learn.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main () {
+    string ch;
+    cin >> ch;
+    cout << "Enter Your name : " << ch << endl;
+
+    int age;
+    cin >> age;
+    cout << "Enter your age : " << age << endl;
+
+    return 0;
+}
