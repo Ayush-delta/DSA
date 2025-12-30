f603f4f0dd7821d32833ffc39ee99957d2674121
Ayush-delta <ayushratna2012@gmail.com>
2025-12-30 23:16:51 +0530

    if-else statement

---
+++ if-else.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    void studentGrade(int marks) {
+        if (marks >= 90) {
+            cout << "Grade A" << endl;
+        } else if(marks >= 70) {
+            cout << "Grade B" << endl;
+        } else if(marks >= 50) {
+            cout << "Grade C" << endl;
+        } else if(marks >= 35) {
+            cout << "Grade D" << endl;
+        } else {
+            cout << "Fail" << endl;
+        }
+
+    }
+};
+
+int main() {
+    Solution sol;
+    int inputMarks;
+
+    cout << "Enter student marks: ";
+    cin >> inputMarks;
+    sol.studentGrade(inputMarks);
+
+    return 0;
+}
