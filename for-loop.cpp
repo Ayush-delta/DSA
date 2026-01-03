acdcf8c2c0bd41ca9fe54f16326adfe675075277
Ayush-delta <ayushratna2012@gmail.com>
2026-01-03 18:35:26 +0530

    Loops

---
+++ for-loop.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // for loop
+    for (int i=5; i>=1; i-=1) {
+        cout << "Hello " << i << endl;
+    }
+
+    //do-while loop
+    // int i = 2 ;
+    // do{
+    //     cout << "Hello " << i<< endl;
+    //     i = i + 1;
+    // } while(i<=1);
+
+    // while loop
+    // int i = 1;
+    // while (i<=5) {
+    //     cout << "Hello" << i << endl;
+    //     i = i + 1;
+    // }
+    return 0;
+}
