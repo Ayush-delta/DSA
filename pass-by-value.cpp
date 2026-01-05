181650524c9538ba84dfa051661a618e8ee49bf5
Ayush-delta <ayushratna2012@gmail.com>
2026-01-05 23:03:58 +0530

    pass-by-value and pass-by-referenc

---
+++ pass-by-value.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+//pass by value
+// void doSomething(string s) {
+//     s[0] = 't';
+//     cout << s << endl;
+// }
+// int main() {
+//     string s = "raj";
+//     doSomething(s);
+//     cout << s << endl;
+
+//     return 0;
+// }
+
+// pass by reference 
+
+void doSomething(string &s) { // original value is passed
+    s[0] = 't'; // modifies the original variable
+    cout << s << endl;
+}
+
+int main() {
+    string s = "raj";
+    doSomething(s);
+    cout << s << endl;
+    return 0;
+}
+
