21554afce1d52bb5eab797e029a20863c2752aa5
Ayush-delta <ayushratna2012@gmail.com>
2026-01-05 18:25:15 +0530

    max-min function

---
+++ sum-of-numbers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+// int sum(int num1, int num2) {
+//     int num3 = num1 + num2;
+//     return num3;
+// }
+// int main() {
+//     int num1 , num2;
+//     cin >> num1 >> num2;
+//     int res = sum(num1, num2);
+//     cout << res;
+//     return 0;
+// }
+
+void sum(int num1, int num2) {
+    int num3 = num1 + num2;
+    cout << num3;
+}
+int main() {
+    int num1 , num2;
+    cin >> num1 >> num2;
+    sum(num1, num2);
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+else return num2;
