3510953dc1202b6f0028fc4df488467b0c15a1e8
Ayush-delta <ayushratna2012@gmail.com>
2026-01-07 11:46:50 +0530

    pass-by-reference-array

--- pass-by-reference-array.cpp
+++ pass-by-reference-array.cpp
@@ -3,18 +3,17 @@ using namespace std;
 
 void doSomething(int arr[] , int n) {
     arr[0] += 100;
-    cout << "Value inside the function: " << arr[0] << endl;
+    cout << "Value inside function: " << arr[0] << endl;
 }
 
-int main() {
+int main () {
     int n = 5;
-    int arr[n];
-    for (int i=0; i<n; i = i + 1) {
+    int arr[5];
+    for (int i=0; i<n ; i+=1){
         cin >> arr[i];
     }
 
     doSomething(arr, n);
-
     cout << "Value inside int main: " << arr[0] << endl;
     return 0;
 }
