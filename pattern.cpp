c07453188c7573e703d5dd9e0fc550c7fb28f3cc
Ayush-delta <ayushratna2012@gmail.com>
2026-01-10 18:11:56 +0530

    pattern 11,12,13,14,1,15,16

--- pattern.cpp
+++ pattern.cpp
@@ -100,6 +100,80 @@ void pattern10(int n) {
         cout << endl;
     }
 }
+
+void pattern11(int n){
+    int start = 1;
+    for (int i = 0; i<n; i++){
+        if ( i % 2 == 0) start = 1;
+        else start = 0;
+        for (int j = 0; j<=i; j++){
+            cout << start << " ";
+            start = 1 - start;
+        }
+        cout << endl;
+    }
+}
+
+void pattern12(int n) {
+    // int space = 2*(n-1);
+    for (int i=1; i<=n ; i++) {
+        //numbers
+        for (int j=1 ; j<=i ; j++){
+            cout << j;
+        }
+        //space
+        for (int j=1; j<=2*(n-i); j++){
+            cout << " ";
+        }
+
+        //numbers
+        for (int j=i ; j>=1; j--){
+            cout << j;
+        }
+        cout << endl;
+        // space -= 2;
+    }
+}
+
+void pattern13(int n) {
+    int num = 1;
+    for (int i=1; i<=n ; i++) {
+        for (int j=1 ; j<=i ; j++){
+            cout << num << " ";
+            num += 1;
+        }
+        cout << endl;
+    }
+}
+
+void pattern14(int n) {
+    for (int i=0; i<n ; i++) {
+        for (char ch ='A' ; ch <= 'A' + i; ch++){
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+void pattern15(int n) {
+    for (int i=0; i<n ; i++) {
+        for (char ch ='A' ; ch <= 'A' + (n-i-1); ch++){
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+void pattern16(int n) {
+    for(int i=0; i<n; i++) {
+        char ch = 'A' + i;
+        for (int j = 0; j<=i; j++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
     int t;
     cin >> t;
@@ -107,7 +181,7 @@ int main() {
         int n;
         cin >> n;
         // pattern7(n);
-        pattern10(n);
+        pattern16(n);
     }
     return 0;
 }
