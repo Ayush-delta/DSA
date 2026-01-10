a9aa2e9d46bc687b7b4b5de8f57730e41d4c202f
Ayush-delta <ayushratna2012@gmail.com>
2026-01-10 12:28:14 +0530

    pattern9,10

--- pattern.cpp
+++ pattern.cpp
@@ -71,13 +71,43 @@ void pattern7(int n){
     }
 }
 
+void pattern8(int n){
+    for (int i = 0; i<n ; i++){
+        for(int j = 0; j<i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j<2*n-(2*i+1); j++){
+            cout << "*";
+        }
+        for(int j = 0; j<i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+//FOr pattern 9 - call both pattern7 and pattern8.
+
+
+void pattern10(int n) {
+    for (int i=1; i<=2*n-1; i++){
+        int stars = i;
+        if(i > n){
+            stars = 2*n-i;
+        }
+        for(int j=1; j<=stars; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
 int main() {
     int t;
     cin >> t;
     for (int i = 0; i < t; i++){
         int n;
         cin >> n;
-        pattern7(n);
+        // pattern7(n);
+        pattern10(n);
     }
     return 0;
 }
