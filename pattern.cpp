1b0e38ab4aa749fdb042f835cd8bab52c7f3946f
Ayush-delta <ayushratna2012@gmail.com>
2026-01-11 22:44:33 +0530

    pattern 19,20,21,22

--- pattern.cpp
+++ pattern.cpp
@@ -207,8 +207,92 @@ void pattern18(int n) {
 
 
 void pattern19(int n) {
-
+int intS = 0;
+        for (int  i = 0; i < n ; i++){
+            //star 
+            for (int j = 1 ; j<=n-i ; j++) {
+                cout << "*";
+            }
+            //space
+            for (int j = 0 ; j<intS; j++) {
+                cout << " ";
+            }
+            //star 
+            for (int j = 1 ; j<=n-i ; j++) {
+                cout << "*";
+            }
+            intS += 2;
+            cout << endl;
+        }
+        intS = 2*(n-1);
+        for (int  i = 1; i <=n ; i++){
+            //star 
+            for (int j = 1 ; j<=i ; j++) {
+                cout << "*";
+            }
+            //space
+            for (int j = 0 ; j<intS; j++) {
+                cout << " ";
+            }
+            //star 
+            for (int j = 1 ; j<=i ; j++) {
+                cout << "*";
+            }
+            intS -= 2;
+            cout << endl;
+        }
 }
+
+void pattern20(int n) {
+
+    int space = 2*(n-1);
+    for (int i = 1; i<=2*n-1; i++){
+        int stars = i;
+        if (i>n) stars = 2*n - i;
+        //stars
+        for (int j=1; j<=stars; j++){
+            cout << "*";
+        }
+        //space
+        for (int j=1; j<=space; j++){
+            cout << " ";
+        }
+        //stars
+        for (int j=1; j<=stars; j++){
+            cout << "*";
+        }
+        cout << endl;
+        if (i<n) space -= 2;
+        else space += 2;
+    }
+}
+
+void pattern21(int n) {
+    for (int i= 0; i<n; i++) {
+        for (int j = 0; j<n; j++){
+            if (i == 0 || j == 0 || i == n-1 || j == n-1){
+                cout << "*";
+            }
+            else cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern22(int n) {
+    for(int i = 0; i<2*n-1; i++){
+        for (int j = 0; j<2*n-1; j++){
+            int top = i;
+            int left = j;
+            int right = 2*(n-1) - j;
+            int bottom = 2*(n-1) - i;
+            cout << (n-min(min(top, bottom) , min(left,right)));
+        }
+        cout << endl;
+    }
+}
+
+
 int main() {
     int t;
     cin >> t;
@@ -216,7 +300,7 @@ int main() {
         int n;
         cin >> n;
         // pattern7(n);
-        pattern18(n);
+        pattern22(n);
     }
     return 0;
 }
