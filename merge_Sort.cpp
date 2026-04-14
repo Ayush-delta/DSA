0fa19cb84bf7c08f92d942e429e8432cb0c574c1
Ayush-delta <ayushratna2012@gmail.com>
2026-04-14 16:53:50 +0530

    merge sort

---
+++ merge_Sort.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    void merge(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+        int l = low;
+        int r = mid+1;
+        while(l <= mid && r <= high) {
+            if(arr[l] <= arr[r]) {
+                temp.push_back(arr[l]);
+                l++;
+            }
+            else {
+                temp.push_back(arr[r]);
+                r++;
+            }
+        }
+            while(l <= mid) {
+                temp.push_back(arr[l]);
+                l++;  
+            }
+            while(r <= high){
+                temp.push_back(arr[r]);
+                r++; 
+            }
+            for(int i = low; i<=high; i++) {
+                arr[i] = temp[i-low];
+            }
+    }
+    void mergeSort(vector<int>& arr, int l, int r) {
+        if (l >= r) return;
+
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+};
