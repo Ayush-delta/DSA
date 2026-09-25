a5b9f9814bcb75f1b70896f78901306f980c76d0
Ayush Ratna <137172098+Ayush-delta@users.noreply.github.com>
2026-09-25 16:28:41 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (46.89%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        int n = nums.size();
+
+        long long max1 = LLONG_MIN;
+        long long max2 = LLONG_MIN;
+        long long max3 = LLONG_MIN;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            if(nums[i] == max1 || nums[i] == max2 || nums[i] == max3) {
+                continue;
+        }
+
+            if(nums[i] > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+        }
+            else if(nums[i] > max2) {
+                max3 = max2;
+                max2 = nums[i];
+        }
+            else if(nums[i] > max3) {
+                max3 = nums[i];
+        }
+    }
+        if(max3 == LLONG_MIN) {
+            return (int)max1;
+        }
+        return (int)max3;
+    }
+};
